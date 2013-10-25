851eaa45cee066f819aceab9c7cdd6f1bc07ea8b
Christian <c.muehle@googlemail.com>
2013-10-25 23:10:39 +0200

    Reverted defaults to normal start

--- Chess/Game/Defaults.cs
+++ Chess/Game/Defaults.cs
@@ -14,10 +14,10 @@ namespace Chess.Game
         public const int  ANY = 2;
         public const UInt64 WhiteKing = 0x8;
         public const UInt64 WhiteQueens = 0x10;
-        public const UInt64 WhiteRooks = 0x100080;//0x81;
-        public const UInt64 WhiteBishops = 0x420;//0x24;
+        public const UInt64 WhiteRooks =  0x81;
+        public const UInt64 WhiteBishops =  0x24;
         public const UInt64 WhiteKnights = 0x42;
-        public const UInt64 WhitePawns = 0x0;//x4FB00;//0xFF00;
+        public const UInt64 WhitePawns = 0xFF00;
         public const UInt64 WhitePieces = 0xFFFF;
 
         public const UInt64 BlackKing = 0x800000000000000;
@@ -25,7 +25,7 @@ namespace Chess.Game
         public const UInt64 BlackRooks = 0x8100000000000000;
         public const UInt64 Blackbishops = 0x2400000000000000;
         public const UInt64 BlackKnights = 0x4200000000000000;
-        public const UInt64 BlackPawns = 0xFC0000000A0000;//0xFF000000000000;
+        public const UInt64 BlackPawns = 0xFF000000000000;
         public const UInt64 BlackPieces = 0xFFFF000000000000;
 
         public const UInt64 Rotated90ClockWise_BlackKing = 0x1000000;
