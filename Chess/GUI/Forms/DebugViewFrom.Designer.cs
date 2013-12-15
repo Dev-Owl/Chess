c4f3cafc00810bb6959270bb158490d56bbeaed6
Christian <c.muehle@googlemail.com>
2013-12-15 18:52:50 +0100

    Added a debug view Form
    This form shows the events and the binary view of the current game

---
+++ Chess/GUI/Forms/DebugViewFrom.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Chess.GUI.Forms
+{
+    partial class DebugViewFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDataView = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDataView
+            // 
+            this.textBoxDataView.Location = new System.Drawing.Point(30, 228);
+            this.textBoxDataView.Multiline = true;
+            this.textBoxDataView.Name = "textBoxDataView";
+            this.textBoxDataView.ReadOnly = true;
+            this.textBoxDataView.Size = new System.Drawing.Size(124, 143);
+            this.textBoxDataView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data view";
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.Location = new System.Drawing.Point(30, 34);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(124, 160);
+            this.listBoxEvents.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Events";
+            // 
+            // DebugViewFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 402);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxEvents);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDataView);
+            this.MaximizeBox = false;
+            this.Name = "DebugViewFrom";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Debug View";
+            this.Load += new System.EventHandler(this.DebugViewFrom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDataView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxEvents;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Chess/GUI/Forms/DebugViewFrom.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chess.Log;
+using Chess.Tools;
+using System.Diagnostics;
+using System.IO;
+using Chess.GUI;
+using Chess.Engine;
+
+
+namespace Chess.GUI.Forms
+{
+    public partial class DebugViewFrom : Form
+    {
+        private GameBoard board;
+
+        public DebugViewFrom(GameBoard Board)
+        {
+            InitializeComponent();
+            this.board = Board;
+            this.board.PropertyChange += board_PropertyChange;
+            this.FormClosing += DebugViewFrom_FormClosing;
+        }
+
+        void DebugViewFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+           
+        }
+
+        void board_PropertyChange(string Event, ChangedEventArgs e)
+        {
+            this.listBoxEvents.Items.Add(Event);
+            switch (Event)
+            {
+                case "New Game":
+                    {
+                        this.textBoxDataView.Text= BitOperations.CreateHumanString(this.board.MoveGenerator.CurrentGameState.SquarsBlocked);
+                    } break;
+                case "Figure selected":
+                    {
+                        this.textBoxDataView.Text = BitOperations.CreateHumanString((UInt64)e.EventData);
+                    } break;
+                case "Figure moved":
+                    {
+                        this.textBoxDataView.Text = BitOperations.CreateHumanString(this.board.MoveGenerator.CurrentGameState.SquarsBlocked);
+                    }
+                    break;
+                case "Move reverted":
+                    {
+                        this.textBoxDataView.Text = BitOperations.CreateHumanString(this.board.MoveGenerator.CurrentGameState.SquarsBlocked);
+                    }break;
+            }
+        }
+
+        private void DebugViewFrom_Load(object sender, EventArgs e)
+        {
+            this.listBoxEvents.Focus();
+        }
+    }
+}
