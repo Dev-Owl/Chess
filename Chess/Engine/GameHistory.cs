3cf7eb2bc2e951cac7f21d196a2ffafb955a9c06
Christian <c.muehle@googlemail.com>
2013-12-15 18:56:22 +0100

    Added game history
    The game keeps track of the changes and is able to revert the lase move

--- Chess/Engine/GameHistory.cs
+++ Chess/Engine/GameHistory.cs
@@ -5,24 +5,65 @@ using System.Text;
 
 namespace Chess.Engine
 {
-    public class GameHistory : List<BitBoard>
+
+    //TODO:Add documentation below
+
+
+    public class GameHistory
     {
-        //TODO: Add meta information to saved data to identify more games (class needed player name color time)
+      
+        private Dictionary<GameInfo,List<BitBoard>> history;
+        private MoveGenerator moveGenerator;
+
+        public MoveGenerator MoveGenerator
+        {
+            get { return moveGenerator; }
+            set { moveGenerator = value; }
+        }
 
         public GameHistory()
-            : base()
-        { }
+        {
+            history = new Dictionary<GameInfo,List<BitBoard>>();
+        }
+
+        public void AddGame( GameInfo Info,List<BitBoard> Moves=null)
+        {
+            this.history.Add(Info,new List<BitBoard>());
+            if( Moves !=null)
+            {
+                this.history[Info].AddRange( Moves);
+            }
+        }
+
+        public void AddHistory(GameInfo Info, BitBoard Move)
+        {
+            this.history[Info].Add(BitBoard.CopyFigureValues(Move));
+        }
 
         public void SaveHistoryToDisk()
         { 
             //TODO: Add a way to save objects on disk
         }
 
-        public void SaveHistoryToDB()
+        public bool RevertLastMove()
         {
-            //TODO: Add a way to save objects on disk
+            bool reverted = false;
+            //Is a moveGenerator set if not we can not revert something
+            if (this.moveGenerator != null)
+            {
+                //Do we have a step that can be removed
+                if (this.history[this.moveGenerator.CurrentGame].Count > 0)
+                { 
+                    //Get history of the game
+                    List<BitBoard> moves = this.history[this.moveGenerator.CurrentGame];
+                    //Set the new state to the moveGenerator
+                    this.moveGenerator.CurrentGameState = moves[moves.Count-1];
+                    //Remove the last entry from the history
+                    moves.RemoveAt(moves.Count - 1);
+                    reverted = true;
+                }
+            }
+            return reverted;
         }
-
-       
     }
 }
