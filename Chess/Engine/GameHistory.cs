00fbb4884ce1a1c89c391c90321aa13eb361a181
Christian <c.muehle@googlemail.com>
2013-12-17 16:32:00 +0100

    Added Save and Load function
    The history is now able to load and save a game from to disk

--- Chess/Engine/GameHistory.cs
+++ Chess/Engine/GameHistory.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Chess.Engine
 {
@@ -11,8 +13,21 @@ namespace Chess.Engine
 
     public class GameHistory
     {
-      
-        private Dictionary<GameInfo,List<BitBoard>> history;
+
+        private Dictionary<GameInfo, List<BitBoard>> history;
+        public Dictionary<GameInfo, List<BitBoard>> History
+        {
+            get { return history; }
+            set { history = value; }
+        }
+
+        private int activeColor;
+        public int ActiveColor
+        {
+            get { return activeColor; }
+            set { activeColor = value; }
+        }
+
         private MoveGenerator moveGenerator;
 
         public MoveGenerator MoveGenerator
@@ -24,6 +39,7 @@ namespace Chess.Engine
         public GameHistory()
         {
             history = new Dictionary<GameInfo,List<BitBoard>>();
+            activeColor = Defaults.WHITE;
         }
 
         public void AddGame( GameInfo Info,List<BitBoard> Moves=null)
@@ -38,11 +54,112 @@ namespace Chess.Engine
         public void AddHistory(GameInfo Info, BitBoard Move)
         {
             this.history[Info].Add(BitBoard.CopyFigureValues(Move));
+            this.activeColor *= -1;
         }
 
-        public void SaveHistoryToDisk()
+        public void SaveHistoryToDisk(GameInfo Info,string PathToSave)
         { 
-            //TODO: Add a way to save objects on disk
+            if (!string.IsNullOrEmpty(PathToSave))
+            {
+                string fileName = Path.GetFileName(PathToSave);
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    fileName = string.Format("{0}vs{1}_{2}.chess", Info.Player1, Info.Player2, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+                    PathToSave = Path.Combine(PathToSave, fileName);
+                }
+                using (StreamWriter sw = new StreamWriter(PathToSave, false, Encoding.UTF8))
+                {
+                    sw.WriteLine( SerializeObject(Info));
+                    sw.WriteLine(history[Info].Count);
+                    foreach (BitBoard board in history[Info])
+                    { 
+                        sw.WriteLine(board.WhiteKing.ToString());
+                        sw.WriteLine(board.WhiteQueens.ToString());
+                        sw.WriteLine(board.WhiteRooks.ToString());
+                        sw.WriteLine(board.WhiteBishops.ToString());
+                        sw.WriteLine(board.WhiteKnights.ToString());
+                        sw.WriteLine(board.WhitePawns.ToString());
+                        sw.WriteLine(board.BlackKing.ToString());
+                        sw.WriteLine(board.BlackQueens.ToString());
+                        sw.WriteLine(board.BlackRooks.ToString());
+                        sw.WriteLine(board.Blackbishops.ToString());
+                        sw.WriteLine(board.BlackKnights.ToString());
+                        sw.WriteLine(board.BlackPawns.ToString());
+                    }
+                    sw.WriteLine(this.activeColor);
+                }
+            }
+        }
+
+        public bool LoadHistoryFromDisk(string PathToLoad)
+        {
+
+            bool result = false;
+            if (!string.IsNullOrEmpty(PathToLoad))
+            {
+                if (File.Exists(PathToLoad))
+                {
+                    this.history.Clear();
+                    using (StreamReader sr = new StreamReader(PathToLoad, Encoding.UTF8))
+                    {
+                        GameInfo info = (GameInfo)DeserializeObject(sr.ReadLine());
+                        BitBoard board = new BitBoard();
+                        this.history.Add(info, new List<BitBoard>());
+                        int amount = 0;
+                        if (int.TryParse(sr.ReadLine(), out amount))
+                        {
+                            for (int step = 0; step < amount; ++step)
+                            {
+                               
+                                board.WhiteKing = UInt64.Parse(sr.ReadLine());
+                                board.WhiteQueens = UInt64.Parse(sr.ReadLine());
+                                board.WhiteRooks = UInt64.Parse(sr.ReadLine());
+                                board.WhiteBishops = UInt64.Parse(sr.ReadLine());
+                                board.WhiteKnights = UInt64.Parse(sr.ReadLine());
+                                board.WhitePawns = UInt64.Parse(sr.ReadLine());
+                                board.BlackKing = UInt64.Parse(sr.ReadLine());
+                                board.BlackQueens = UInt64.Parse(sr.ReadLine());
+                                board.BlackRooks = UInt64.Parse(sr.ReadLine());
+                                board.Blackbishops = UInt64.Parse(sr.ReadLine());
+                                board.BlackKnights = UInt64.Parse(sr.ReadLine());
+                                board.BlackPawns = UInt64.Parse(sr.ReadLine());
+                                this.history[info].Add(BitBoard.CopyFigureValues(board));
+                            }
+                            this.activeColor = int.Parse(sr.ReadLine());
+                            result = true;
+                        }
+                        else
+                        {
+                            throw new Exception("Invalid save file");
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private string SerializeObject(object o)
+        {
+            if (!o.GetType().IsSerializable)
+            {
+                return null;
+            }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(stream, o);
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+
+        private object DeserializeObject(string str)
+        {
+            byte[] bytes = Convert.FromBase64String(str);
+
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                return new BinaryFormatter().Deserialize(stream);
+            }
         }
 
         public bool RevertLastMove()
