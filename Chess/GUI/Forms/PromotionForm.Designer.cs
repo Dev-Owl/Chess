f8dbf4ea7a2ab7b28f5812882a3c211c6806fd29
Christian <c.muehle18@googlemail.com>
2013-12-22 18:37:36 +0100

    Added Promotion to GUI
    The gui is now able to show a form and ask for the new figure

---
+++ Chess/GUI/Forms/PromotionForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Chess.GUI.Forms
+{
+    partial class PromotionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Queen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(98, 23);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 68);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Rook";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 68);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Bishop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(270, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 68);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Knight";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // PromotionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 113);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PromotionForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Promotion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Chess/GUI/Forms/PromotionForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Chess.Engine;
+
+namespace Chess.GUI.Forms
+{
+    public partial class PromotionForm : Form
+    {
+
+       
+        public EFigures Result { get; set; }
+
+        public PromotionForm()
+        {
+            InitializeComponent();
+            Result = EFigures.NAN;
+        }
+
+        private void SaveAndClose(EFigures Result)
+        {
+            this.Result = Result;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.SaveAndClose(EFigures.Queen);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.SaveAndClose(EFigures.Rook);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.SaveAndClose(EFigures.Bishop);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.SaveAndClose(EFigures.Knight);
+        }
+
+    }
+}
