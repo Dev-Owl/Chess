c5ce7cb0f6fc11027ef54dc2182cdccc07d672e1
Christian <c.muehle18@googlemail.com>
2013-12-22 18:36:53 +0100

    Added Promotion support
    The engine is now able to detected a promotion and request ( if the
    needed interface is provided) the new figure. If not the game takes a
    queen as new figure

---
+++ Chess/Engine/IPromotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess.Engine
+{
+    public interface IPromotion
+    {
+        EFigures GetDecision();
+    }
+}
